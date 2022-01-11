8f87f45bfbae65569c6331f52b56f89cbe5d5394
Alex <alexlisecky@gmail.com>
2022-01-11 21:28:23 +0300

    add geekshop

---
+++ geekshop/mainapp/context_processors/context_processors.py
@@ -0,0 +1,12 @@
+from basketapp.models import Basket
+
+def basket(request):
+   print(f'context processor basket works')
+   basket = []
+
+   if request.user.is_authenticated:
+       basket = request.user.basket.select_related()
+
+   return {
+       'basket': basket,
+   }
